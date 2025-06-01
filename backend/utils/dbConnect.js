7c681d9df48ca1561460bec5dc2a657c2d483818
metrixhunter <diamondlakshya1976@gmail.com>
2025-06-01 17:30:54 +0530

    My local changes to dbConnect.js

--- backend/utils/dbConnect.js
+++ backend/utils/dbConnect.js
@@ -1,13 +1,5 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI;
-
-if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  );
-}
-
 /**
  * Global is used here to maintain a cached connection across hot reloads in development.
  */
@@ -20,6 +12,14 @@ if (!cached) {
 async function dbConnect() {
   if (cached.conn) {
     return cached.conn;
+  }
+
+  const MONGODB_URI = process.env.MONGODB_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error(
+      'Please define the MONGODB_URI environment variable inside .env.local or in your Vercel project settings'
+    );
   }
 
   if (!cached.promise) {
